7ed2be0fb1a2999512904e1fae98215e89bcbcec
Rizki Andika Setiadi <106527864+99kairas@users.noreply.github.com>
2023-10-16 21:01:04 +0700

    first commit

---
+++ configs/config.go
@@ -0,0 +1,58 @@
+package configs
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+type Config struct {
+	DB_Username string
+	DB_Password string
+	DB_Port     string
+	DB_Host     string
+	DB_Name     string
+}
+
+func InitDB() *gorm.DB {
+	loc, _ := time.LoadLocation("Asia/Jakarta")
+	time.Local = loc
+
+	config := Config{
+		DB_Username: os.Getenv("DB_Username"),
+		DB_Password: os.Getenv("DB_Password"),
+		DB_Port:     os.Getenv("DB_Port"),
+		DB_Host:     os.Getenv("DB_Host"),
+		DB_Name:     os.Getenv("DB_Name"),
+	}
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True",
+		config.DB_Username,
+		config.DB_Password,
+		config.DB_Host,
+		config.DB_Port,
+		config.DB_Name,
+	)
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{
+		TranslateError: true,
+	})
+
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	InitMigrate()
+
+	return DB
+}
+
+func InitMigrate() {
+
+}
